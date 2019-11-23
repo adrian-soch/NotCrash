cf39b78c34cb3a4d1946705a2d37f3fe9ee47638
Adrian Sochaniwsky <adr.soch@outlook.com>
2019-11-23 08:45:04 -0500

    Added comments to alertnessDetectorTest

--- scripts/alertnessDetectorTest.py
+++ scripts/alertnessDetectorTest.py
@@ -7,95 +7,109 @@ import winsound
 
 from scipy.spatial import distance
 from imutils import face_utils
-#from imutils import VideoStream
 from threading import Thread
 
-EYE_AR_THRESH = 0.3
-EYE_AR_CONSEC_FRAMES = 48
+EYE_AR_THRESH = 0.28
+EYE_AR_CONSEC_FRAMES = 40
 
 COUNTER = 0
 ALARM_ON = False
 
-frequency = 3000  # Set Frequency (Hz)
-duration = 1000  # Set Duration (ms)
+frequency = 2750  # Set Frequency (Hz)
+duration = 333  # Set Duration (ms)
 
 def alarm():
-    #Windows machine emits the specific frequency for the specified duration
+    # Windows machine emits the specific frequency for the specified duration
     winsound.Beep(frequency, duration)
 
-def eyeAspectRatio(eye):
+def eye_aspect_ratio(eye):
     A = distance.euclidean(eye[1], eye[5])
     B = distance.euclidean(eye[2], eye[4])
     #Euclidean distance
     C = distance.euclidean(eye[0], eye[3]) 
 	#Eye aspect ratio
-    eyeAspect = (A + B) / (2.0 * C)
+    ear = (A + B) / (2.0 * C)
 	
-    return eyeAspect
+    return ear
+
+# Initialize dlib face detector
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
+# grab the indexes of the facial landmarks for the left and
+# right eye, respectively
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
-# 0 for device webcam default
-# 1 for external webcam
-vs = cv.VideoCapture(1)
+# Start video stream
+vs = vs = cv.VideoCapture(1)
+time.sleep(0.3)
 
-if not vs.isOpened():
-    vs.release()
-
+# loop over frames from the video stream
 while True:
 
+    # Resize recolour captured frame
+    ret, frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-        # Delay for camera to stabilize
-        time.sleep(1.0)
-        #Take a frame from the video
-        ret, frame = vs.read()
-        #resize
-        frame = imutils.resize(frame,width=450)
-        #
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	# Detect face(s)
+    rects = detector(gray, 0)
 
-        rects = detector(gray, 0)
+	# loop through faces
+    for rect in rects:
+		
+        # Get facial landmarks
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
 
-        for rect in rects:
-            shape = predictor(gray, rect)
-            shape = face_utils.shape_to_np(shape)
-            
-            leftEye = shape[lStart:lEnd]
-            rightEye = shape[rStart:rEnd]
-            leftEAR = eyeAspectRatio(leftEye)
-            rightEAR = eyeAspectRatio(rightEye)
-            eyeAspect = (leftEAR + rightEAR)/2.0
-            
-            leftEyeHull = cv.convexHull(leftEye)
-            rightEyeHull = cv.convexHull(rightEye)
-            cv.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-            cv.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-            
-            if eyeAspect < EYE_AR_THRESH:
-                COUNTER += 1
-                
-                if COUNTER >= EYE_AR_CONSEC_FRAMES:
-                    # if the alarm is not on, turn it on
-                    if not ALARM_ON:
-                        ALARM_ON = True
+        # Get aspect ratios
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
 
-                        # sound played in the background
-                        t = Thread(target=alarm)
-                        t.start()
-                    cv.putText(frame, "ALERT", (10, 30),cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-            else:
-                COUNTER = 0
-                ALARM_ON = False
-                cv.putText(frame, "EAR: {:.2f}".format(eyeAspect), (300, 30),cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    
-        # Display output
-        cv.imshow("Frame", frame)
-        if cv.waitKey(1) >= 0:
-            vs.release()
-            break
-#Close remaining windows
+		# Average ear value
+        ear = (leftEAR + rightEAR) / 2.0
+
+        leftEyeHull = cv.convexHull(leftEye)
+        rightEyeHull = cv.convexHull(rightEye)
+
+        # Display outline of detected eyes
+        cv.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)   
+        cv.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+		# Checking ear ratio
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+
+            # Sounds alarm after specified counter overflow
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+				# Turns alarm on
+                if not ALARM_ON:
+                    ALARM_ON = True
+
+                    #Plays winsound in background
+                    t = Thread(target=alarm)
+                    t.daemon = True
+                    t.start()
+
+			# Prints alert
+            cv.putText(frame, "ALERT", (10, 30),cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        else:
+            COUNTER = 0
+            ALARM_ON = False
+
+        # Prints current ear value
+        cv.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+ 
+	# Opens window
+    cv.imshow("Frame", frame)
+    #Any key breaks loop
+    if cv.waitKey(1) >= 0:
+        vs.release()
+        break
+# Destroy any leftover windows
 cv.destroyAllWindows()
