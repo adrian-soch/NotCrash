4b2d0297f2ef8dc74be21b0534cd5004b5450c32
Adrian Sochaniwsky <adr.soch@outlook.com>
2019-11-23 12:31:31 -0500

    Added threading

--- scripts/alertnessDetectorTest.py
+++ scripts/alertnessDetectorTest.py
@@ -8,6 +8,7 @@ import winsound
 from scipy.spatial import distance
 from imutils import face_utils
 from threading import Thread
+from multiprocessing import Process
 
 EYE_AR_THRESH = 0.28
 EYE_AR_CONSEC_FRAMES = 38
@@ -32,11 +33,148 @@ def eye_aspect_ratio(eye):
 	
     return ear
 
-def closedEyeDetector(vs):
-    
-    # Initialize dlib face detector
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+def headPoseEst(vs, detector, predictor):
+    image_points = np.array([
+                            (359, 391),     # Nose tip 34
+                            (399, 561),     # Chin 9
+                            (337, 297),     # Left eye left corner 37
+                            (513, 301),     # Right eye right corne 46
+                            (345, 465),     # Left Mouth corner 49
+                            (453, 469)      # Right mouth corner 55
+                        ], dtype="double")
+
+    # 3D model points.
+    model_points = np.array([
+                            (0.0, 0.0, 0.0),             # Nose tip 34
+                            (0.0, -330.0, -65.0),        # Chin 9
+                            (-225.0, 170.0, -135.0),     # Left eye left corner 37
+                            (225.0, 170.0, -135.0),      # Right eye right corne 46
+                            (-150.0, -150.0, -125.0),    # Left Mouth corner 49
+                            (150.0, -150.0, -125.0)      # Right mouth corner 55
+
+                        ])
+
+    while True:
+
+        if not vs.isOpened():
+            vs.release()
+            print("Camera Error")
+            break
+
+        ret, frame = vs.read()
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        frame = imutils.resize(frame, width=1024, height=576)
+        size = gray.shape
+
+        # detect faces in the grayscale frame
+        rects = detector(gray, 0)
+
+        # check to see if a face was detected, and if so, draw the total
+        # number of faces on the frame
+        if len(rects) > 0:
+                text = "{} face(s) found".format(len(rects))
+                cv.putText(frame, text, (10, 20), cv.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 255), 2)
+
+        # loop over the face detections
+        for rect in rects:
+                # compute the bounding box of the face and draw it on the
+                # frame
+                        (bX, bY, bW, bH) = face_utils.rect_to_bb(rect)
+                        cv.rectangle(frame, (bX, bY), (bX + bW, bY + bH),(0, 255, 0), 1)
+                # determine the facial landmarks for the face region, then
+                # convert the facial landmark (x, y)-coordinates to a NumPy
+                # array
+                        shape = predictor(gray, rect)
+                        shape = face_utils.shape_to_np(shape)
+                # loop over the (x, y)-coordinates for the facial landmarks
+                # and draw each of them
+                        for (i, (x, y)) in enumerate(shape):
+                                if i == 33:
+                                        #something to our key landmarks
+                    # save to our new key point list
+                    # i.e. keypoints = [(i,(x,y))]
+                                        image_points[0] = np.array([x,y],dtype='double')
+                    # write on frame in Green
+                                        cv.circle(frame, (x, y), 1, (0, 255, 0), -1)
+                                        cv.putText(frame, str(i + 1), (x - 10, y - 10),cv.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 0), 1)
+                                elif i == 8:
+                    #something to our key landmarks
+                    # save to our new key point list
+                    # i.e. keypoints = [(i,(x,y))]
+                                        image_points[1] = np.array([x,y],dtype='double')
+                    # write on frame in Green
+                                        cv.circle(frame, (x, y), 1, (0, 255, 0), -1)
+                                        cv.putText(frame, str(i + 1), (x - 10, y - 10),cv.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 0), 1)
+                                elif i == 36:
+                                        #something to our key landmarks
+                                        # save to our new key point list
+                                        # i.e. keypoints = [(i,(x,y))]
+                                        image_points[2] = np.array([x,y],dtype='double')
+                                        # write on frame in Green
+                                        cv.circle(frame, (x, y), 1, (0, 255, 0), -1)
+                                        cv.putText(frame, str(i + 1), (x - 10, y - 10),cv.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 0), 1)
+                                elif i == 45:
+                                        #something to our key landmarks
+                    # save to our new key point list
+                    # i.e. keypoints = [(i,(x,y))]
+                                        image_points[3] = np.array([x,y],dtype='double')
+                    # write on frame in Green
+                                        cv.circle(frame, (x, y), 1, (0, 255, 0), -1)
+                                        cv.putText(frame, str(i + 1), (x - 10, y - 10),cv.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 0), 1)
+                                elif i == 48:
+                    #something to our key landmarks
+                    # save to our new key point list
+                    # i.e. keypoints = [(i,(x,y))]
+                                        image_points[4] = np.array([x,y],dtype='double')
+                    # write on frame in Green
+                                        cv.circle(frame, (x, y), 1, (0, 255, 0), -1)
+                                        cv.putText(frame, str(i + 1), (x - 10, y - 10),cv.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 0), 1)
+                                elif i == 54:
+                    #something to our key landmarks
+                    # save to our new key point list
+                    # i.e. keypoints = [(i,(x,y))]
+                                        image_points[5] = np.array([x,y],dtype='double')
+                    # write on frame in Green
+                                        cv.circle(frame, (x, y), 1, (0, 255, 0), -1)
+                                        cv.putText(frame, str(i + 1), (x - 10, y - 10),cv.FONT_HERSHEY_SIMPLEX, 0.35, (0, 255, 0), 1)
+                                else:
+                    #everything to all other landmarks
+                    # write on frame in Red
+                                        cv.circle(frame, (x, y), 1, (0, 0, 255), -1)
+                                        cv.putText(frame, str(i + 1), (x - 10, y - 10),cv.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+                        focal_length = size[1]
+                        center = (size[1]/2, size[0]/2)
+                        camera_matrix = np.array([[focal_length,0,center[0]],[0, focal_length, center[1]],[0,0,1]], dtype="double")
+
+                        #print "Camera Matrix :\n {0}".format(camera_matrix)
+
+                        dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+                        (success, rotation_vector, translation_vector) = cv.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv.SOLVEPNP_ITERATIVE)#flags=cv.CV_ITERATIVE)
+
+                        #print "Rotation Vector:\n {0}".format(rotation_vector)
+                        #print "Translation Vector:\n {0}".format(translation_vector)
+                        # Project a 3D point (0, 0 , 1000.0) onto the image plane
+                        # We use this to draw a line sticking out of the nose_end_point2D
+                        (nose_end_point2D, jacobian) = cv.projectPoints(np.array([(0.0, 0.0, 1000.0)]),rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+                        for p in image_points:
+                                cv.circle(frame, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
+
+                        p1 = ( int(image_points[0][0]), int(image_points[0][1]))
+                        p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+
+                        cv.line(frame, p1, p2, (255,0,0), 2)
+        # Opens window
+        cv.imshow("Frame2", frame)
+        #Any key breaks loop
+        if cv.waitKey(1) >= 0:
+            vs.release()
+            break
+    # Destroy any leftover windows
+    cv.destroyAllWindows()
+
+ 
+def closedEyeDetector(vs, detector, predictor):
 
     # grab the indexes of the facial landmarks for the left and
     # right eye, respectively
@@ -115,7 +253,12 @@ def closedEyeDetector(vs):
     # Destroy any leftover windows
     cv.destroyAllWindows()
 
+
 def main():
+
+    # Initialize dlib face detector
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
     # Start video stream
     vs = cv.VideoCapture(1)
@@ -123,8 +266,10 @@ def main():
         vs = cv.VideoCapture(0)
     time.sleep(0.3)
     
-    closedEyeDetector(vs)
-
-
+    #Thread(target = closedEyeDetector(vs, detector, predictor)).start
+    #Thread(target = headPoseEst(vs, detector, predictor)).start
+    Process(target = closedEyeDetector(vs, detector, predictor),daemon=True).start
+    Process(target = headPoseEst(vs, detector, predictor)).start
+    
 if __name__ == '__main__':
     main()
